8fcfef224425727c2a27edebaa4e609e3b0d81aa
mee-1326 <meetraw6597@gmail.com>
2022-04-26 22:10:25 +0530

    Initial commit

---
+++ app/src/main/java/com/raw/resumemaker/refrence.java
@@ -0,0 +1,16 @@
+package com.raw.resumemaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class refrence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refrence);
+
+
+    }
+}
